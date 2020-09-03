b967af94425a6ef500cc78acd07dfc4d8f0f549f
Tyler <gte043@gmail.com>
2020-09-03 11:38:44 -0500

    Application completed, everything working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 from geopy.geocoders import Nominatim
 import csv
+from flask import send_file, send_from_directory, safe_join, abort
 
 geolocater= Nominatim(user_agent="GeocoderWebApp", timeout=1000)
 
@@ -13,69 +14,67 @@ def index():
 
 @app.route("/data", methods=['GET','POST'])
 def data():
-
     if request.method == "POST":
         UserInput=request.form['csvfile']
+        df = pd.read_csv(UserInput)
+        columntitles=df.columns.tolist()
+        checktitles=['Address', 'address']
+        #Checking if Address and or address column is present in spreadsheet
+        def common_data(columntitles, checktitles):
+            for x in columntitles:
+                for y in checktitles:
+                    if x==y:
+                        result=True
+                        return result
+                        break
+        titleOutput=(common_data(columntitles,checktitles))
 
+        if titleOutput != None:
 
-    df = pd.read_csv(UserInput)
-    columntitles=df.columns.tolist()
-    checktitles=['Address', 'address']
+            #Sort user spreadsheet for Address/address values
+            for title in columntitles:
+                if title == "Address":
+                    C=df['Address']
+                elif title == "address":
+                    C=df['address']
+                else:
+                    pass
 
-    #Checking if Address and or address column is present in spreadsheet
-    def common_data(columntitles, checktitles):
-        for x in columntitles:
-            for y in checktitles:
-                if x==y:
-                    result=True
-                    return result
-                    break
+            #setup two empty lists for the Longitude and Latitude values
+            rows2=[]
+            rows3=[]
 
-    #Running Header Definition      
-    titleOutput=(common_data(columntitles,checktitles))
+            # Loop for geolocating all addresses from spreadsheet
+            for i in C:
+                location=geolocater.geocode(i)
+                longitudecoordinate=location.longitude
+                latitudecoordinate=location.latitude
+                rows2.append(longitudecoordinate)
+                rows3.append(latitudecoordinate) 
+                print(location.longitude, location.latitude)
 
-    #define what is the exact header match between the two lists
-    same_values=set(columntitles) & set(checktitles)
+            #Create Pandas dataframes from rows objects
+            df2=pd.DataFrame(rows2,columns=['Longitude'])
+            df3=pd.DataFrame(rows3,columns=['Latitude'])
 
-    #Checking header function output, true or false. If false, print message for user
-    if titleOutput == None:
-        print("Please insert appropriate file")
+            #Merge dataframes as new columns to user excel file
+            df['Longitude']=df2
+            df['Latitude']=df3
 
-    #Sort user spreadsheet for Address/address values
-    for title in columntitles:
-        if title == "Address":
-            C=df['Address']
-        elif title == "address":
-            C=df['address']
-        else:
-            pass
+            #Convert dataframe to excel sheet for user to download
+            df.to_csv ('exportDataframe.csv', index = False, header=True)
 
-    #setup two empty lists for the Longitude and Latitude values
-    rows2=[]
-    rows3=[]
+            #Convert df to html table for displaying
+            HTMLTable=df.to_html(header="true", table_id="table")
 
-    # Loop for geolocating all addresses from spreadsheet
-    for i in C:
-        location=geolocater.geocode(i)
-        longitudecoordinate=location.longitude
-        latitudecoordinate=location.latitude
-        rows2.append(longitudecoordinate)
-        rows3.append(latitudecoordinate) 
-        print(location.longitude, location.latitude)
+            return render_template('data.html', data=HTMLTable)
+    return render_template('index.html', text="Please insert appropriate file")
 
-    #Create Pandas dataframes from rows objects
-    df2=pd.DataFrame(rows2,columns=['Longitude'])
-    df3=pd.DataFrame(rows3,columns=['Latitude'])
-
-    #Merge dataframes as new columns to user excel file
-    df['Longitude']=df2
-    df['Latitude']=df3
-
-    #Convert dataframe to excel sheet for user to download
-    df.to_csv ('exportDataframe.csv', index = False, header=True)
-
-    return render_template('data.html')
-
+#Generating new page for downloading CSV file
+@app.route('/download', methods=['GET', 'POST'])
+def downloadfile():
+        path = "exportDataframe.csv"
+        return send_file(path, as_attachment=True)
 
 if __name__== '__main__':
     app.debug=True
